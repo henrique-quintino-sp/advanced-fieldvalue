75de6c69f8ddf6b8a097ec9d968df2ae5ec20d7c
henrique-quintino-sp <henrique.quintino@sailpoint.com>
2017-07-11 16:51:38 -0500

    form adjust

--- src/forms/FormMain.java
+++ src/forms/FormMain.java
@@ -6,8 +6,12 @@ import sailpoint.object.*;
 import sailpoint.tools.GeneralException;
 
 import javax.swing.*;
+import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,32 +25,124 @@ public class FormMain {
     private JList listIdentity;
     private JTable tableResult;
     private JButton xButton;
-    private JTextField textField1;
+    private JTextField txtCustom;
     private JButton generateButton;
     private JButton loadButton;
     private JButton connectButton;
+    private JCheckBox checkUnique;
+    private JButton button2;
 
     private SailPointContext spContext;
+    private String currentApplication;
+    private String DESTINATION_FOLDER= "Field Value Files";
+    DefaultTableModel tableModel;
 
     public FormMain() {
+
         loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 loadSchema();
+                loadDefaultTable();
             }
         });
 
         connectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(e.getActionCommand());
+
                 loadContext();
                 loadApplications();
 
                 loadIdentityAttributes();
+
+                loadDefaultTable();
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                recordSelection();
+            }
+        });
+        xButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteTableRow();
+            }
+        });
+        generateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generateFieldValueXml();
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader("iiq.properties"));
+
+                    String line = reader.readLine();
+                    while(line != null)
+                    {
+                        if(line.startsWith("dataSource.url"))
+                        {
+                            System.out.println(line);
+                            break;
+                        }
+                        line = reader.readLine();
+                    }
+                    reader.close();
+                }catch (Exception err){
+                    System.out.println(err.getMessage());
+
+                }
             }
         });
     }
+
+    private void generateFieldValueXml() {
+        //GenerateFieldValuesXML genXml = new GenerateFieldValuesXML(currentApplication,null,null);
+        //genXml.writeXML();
+    }
+
+    private void recordSelection() {
+        if(validSelection()) {
+            String schemaSelected = listSchema.getSelectedValue().toString();
+            String identitySelected = txtCustom.getText();
+            if(identitySelected == null || identitySelected.isEmpty())
+                identitySelected = listIdentity.getSelectedValue().toString();
+            Boolean unique = checkUnique.getSelectedObjects() != null;
+            String[] row = {schemaSelected, identitySelected, unique.toString()};
+
+            tableModel.addRow(row);
+
+            clearSelection();
+        }
+    }
+
+    private boolean validSelection() {
+        ArrayList<String> arrErrors = new ArrayList<>();
+
+        if(listSchema.getSelectedValue() == null)
+            arrErrors.add(" * Invalid a schema item");
+        if(listIdentity.getSelectedValue() == null && txtCustom.getText().isEmpty())
+            arrErrors.add(" * Invalid Identity target or expression");
+
+        if(arrErrors.size() > 0)
+        {
+            String errorMsg="The following error(s) were found:\n\n";
+            for(String s : arrErrors )
+                errorMsg += s + "\n";
+
+            showErrorMsg(errorMsg,"Invalid selection");
+        }
+
+        return arrErrors.size() == 0;
+    }
+
+
 
     private void loadIdentityAttributes() {
         try {
@@ -61,10 +157,8 @@ public class FormMain {
 
         }catch (GeneralException err)
         {
-            System.out.println(err.getMessage());
+            showErrorMsg("Problem loading Identity attributes \n" + err.getMessage(),"Error loading Identity Attributes");
         }
-
-
     }
 
     private void loadSchema() {
@@ -77,11 +171,12 @@ public class FormMain {
             listSchema.setModel(listModel);
             for(String s : schema.getAttributeNames())
                 listModel.addElement(s);
-
+            currentApplication = cbApplication.getSelectedItem().toString();
         }
         catch (GeneralException err)
         {
-            System.out.println("Exception ok - " + err.getMessage());
+            showErrorMsg("Problem loading Application Schema \n" + err.getMessage(),"Error loading Application Schema");
+
         }
     }
 
@@ -110,7 +205,7 @@ public class FormMain {
             //spContext.close();
         }catch (GeneralException err)
         {
-            System.out.println("Error " + err.getMessage());
+            showErrorMsg("Problem loading Application \n" + err.getMessage(),"Error loading Application");
         }
     }
 
@@ -124,7 +219,32 @@ public class FormMain {
         }
         catch (GeneralException err)
         {
-            System.err.println("Error in Connecting - " + err.getMessage());
+            showErrorMsg("Problem connecting to IIQ \n" + err.getMessage(),"Error connecting to IIQ");
         }
+    }
+
+    private void clearSelection() {
+        checkUnique.setSelected(false);
+        txtCustom.setText("");
+        listIdentity.clearSelection();
+        listSchema.clearSelection();
+    }
+    private void showErrorMsg(String message,String title) {
+        JOptionPane.showMessageDialog(panel1, message,title,JOptionPane.ERROR_MESSAGE);
+    }
+    private void deleteTableRow() {
+        int selectedRow = tableResult.getSelectedRow();
+        if(selectedRow > -1)
+        {
+            tableModel.removeRow(selectedRow);
+        }
+    }
+    private void loadDefaultTable() {
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Application");
+        tableModel.addColumn("Identity");
+        tableModel.addColumn("Unique");
+        tableResult.setModel(tableModel);
+        tableResult.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     }
 }
